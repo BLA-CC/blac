ef7b2e4136423e0c2bd49cf54b68da30f9dc6f1f
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2024-09-25 16:22:56 -0300

    Lexer tests added

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,5 @@
 #include <gtest/gtest.h>
 
-TEST(test_suite_name, demo) {
-    ASSERT_EQ(1, 1);
-    ASSERT_TRUE(1);
-    ASSERT_STREQ("hola", "hola");
-}
-
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
