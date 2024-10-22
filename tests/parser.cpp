67681ffd7325f0b5f306c9c1157af479211ebd44
alvaro-sch <alvarolegit@gmail.com>
2024-10-22 17:17:31 -0300

    parser: fix warning and run formatter

--- tests/parser.cpp
+++ tests/parser.cpp
@@ -14,7 +14,7 @@ class ParserTest : public ::testing::Test {
 
     void SetUp() override {
         str_pool = { 0 };
-        yylex_init_extra(&str_pool, &scanner); 
+        yylex_init_extra(&str_pool, &scanner);
     }
 
     void TearDown() override {
@@ -22,7 +22,7 @@ class ParserTest : public ::testing::Test {
     }
 
     int ParseString(const char *input, Ast *ast) {
-        parser = { 0 };
+        parser = { { 0 } };
         YY_BUFFER_STATE buffer = yy_scan_string(input, scanner);
         int result = yyparse(&parser, scanner);
 
@@ -35,8 +35,9 @@ class ParserTest : public ::testing::Test {
         return result;
     }
 
-    AstNode Root(Ast ast) { return ast.nodes[0]; }
-
+    AstNode Root(Ast ast) {
+        return ast.nodes[0];
+    }
 };
 
 TEST_F(ParserTest, ParseProgram) {
@@ -50,29 +51,33 @@ TEST_F(ParserTest, ParseProgram) {
 TEST_F(ParserTest, ExampleProgram) {
     Ast ast;
     ASSERT_EQ(
-        ParseString("program {\n"
-                    "    void main() {\n"
-                    "        integer y = 0;\n"
-                    "        y = getint();\n"
-                    "        if (y == 1) then {\n"
-                    "            printint(y);\n"
-                    "        } else {\n"
-                    "            return printint(inc(y));\n"
-                    "        }\n"
-                    "    }\n"
-                    "}\n", &ast),
+        ParseString(
+            "program {\n"
+            "    void main() {\n"
+            "        integer y = 0;\n"
+            "        y = getint();\n"
+            "        if (y == 1) then {\n"
+            "            printint(y);\n"
+            "        } else {\n"
+            "            return printint(inc(y));\n"
+            "        }\n"
+            "    }\n"
+            "}\n",
+            &ast),
         0);
 }
 
 TEST_F(ParserTest, ProgramBody) {
     Ast ast;
     ASSERT_EQ(
-        ParseString(" program {\n"
-                    "     integer ivar = 0;\n"
-                    "     integer method(integer param){ return; }\n"
-                    "     bool bvar = 0;\n"
-                    "     void external() extern;\n"
-                    " }\n", &ast),
+        ParseString(
+            " program {\n"
+            "     integer ivar = 0;\n"
+            "     integer method(integer param){ return; }\n"
+            "     bool bvar = 0;\n"
+            "     void external() extern;\n"
+            " }\n",
+            &ast),
         0);
     ASSERT_EQ(ast.len, 17);
 
@@ -83,19 +88,21 @@ TEST_F(ParserTest, ProgramBody) {
 TEST_F(ParserTest, ParseVariableDecl) {
     Ast ast;
     ASSERT_EQ(
-        ParseString(" program {\n"
-                    "     bool bvar = true;\n"
-                    "     integer ivar = 0;\n"
-                    " }\n", &ast),
+        ParseString(
+            " program {\n"
+            "     bool bvar = true;\n"
+            "     integer ivar = 0;\n"
+            " }\n",
+            &ast),
         0);
 
     ASSERT_EQ(ast.len, 7);
 
-    AstNodeFull_VarDecl var_decl = Ast_full_var_decl(ast, Ast_full_prog(ast).begin);
+    AstNodeFull_VarDecl var_decl =
+        Ast_full_var_decl(ast, Ast_full_prog(ast).begin);
     ASSERT_EQ(var_decl.type, Type_BOOL);
     ASSERT_EQ(var_decl.ident, StrPool_put(&str_pool, "bvar"));
     ASSERT_NE(var_decl.init_expr, NO_NODE);
-
 
     ASSERT_NE(ParseString("program { bool bvar; }", &ast), 0);
     ASSERT_NE(ParseString("program { bool; }", &ast), 0);
@@ -110,91 +117,88 @@ TEST_F(ParserTest, ParseMethodDecl) {
             "     integer func1(integer a) { integer foo = 2; return; }\n"
             "     bool func2() { return true; }\n"
             "     void print(integer i) extern;\n"
-            " }\n", &ast),
+            " }\n",
+            &ast),
         0);
 
     ASSERT_EQ(ast.len, 33);
-    AstNodeFull_MethDecl meth = Ast_full_meth_decl(ast, Ast_full_prog(ast).begin);
+    AstNodeFull_MethDecl meth =
+        Ast_full_meth_decl(ast, Ast_full_prog(ast).begin);
 
     ASSERT_EQ(meth.ident, StrPool_put(&str_pool, "main"));
     ASSERT_EQ(meth.ret_type, Type_VOID);
 
     NodeIdx params_begin = ast.nodes[meth.params].data.lhs;
 
-    ASSERT_EQ(
-        ast.nodes[params_begin].data.lhs,
-        Type_INT
-    );
+    ASSERT_EQ(ast.nodes[params_begin].data.lhs, Type_INT);
+
+    ASSERT_EQ(ast.nodes[params_begin].data.rhs, StrPool_put(&str_pool, "a"));
+
+    ASSERT_EQ(ast.nodes[params_begin + 1].data.lhs, Type_BOOL);
 
     ASSERT_EQ(
-        ast.nodes[params_begin].data.rhs,
-        StrPool_put(&str_pool, "a")
-    );
-
-    ASSERT_EQ(
-        ast.nodes[params_begin + 1 ].data.lhs,
-        Type_BOOL
-    );
-
-    ASSERT_EQ(
-        ast.nodes[params_begin + 1].data.rhs,
-        StrPool_put(&str_pool, "b")
-    );
+        ast.nodes[params_begin + 1].data.rhs, StrPool_put(&str_pool, "b"));
 }
 
 TEST_F(ParserTest, ParseBlock) {
     Ast ast;
     ASSERT_EQ(
-        ParseString(" program {\n"
-                    "     void main() {\n"
-                    "         integer x = 10;\n"
-                    "         bool w = false;\n"
-                    "         x = true;\n"
-                    "         w = true;\n"
-                    "     }\n"
-                    " }\n", &ast),
+        ParseString(
+            " program {\n"
+            "     void main() {\n"
+            "         integer x = 10;\n"
+            "         bool w = false;\n"
+            "         x = true;\n"
+            "         w = true;\n"
+            "     }\n"
+            " }\n",
+            &ast),
         0);
 
     ASSERT_NE(
-        ParseString(" program {\n"
-                    "     void main() {\n"
-                    "         void func(integer a) {}\n"
-                    "     }\n"
-                    " }\n", &ast),
+        ParseString(
+            " program {\n"
+            "     void main() {\n"
+            "         void func(integer a) {}\n"
+            "     }\n"
+            " }\n",
+            &ast),
         0);
 }
-
 
 TEST_F(ParserTest, ParseStatements) {
     Ast ast;
     ASSERT_EQ(
-        ParseString(" program {\n"
-                    "     void main() {\n"
-                    "         integer x = 0;\n"
-                    "         integer y = 100 + 32;\n"
-                    "         x = 10;\n"
-                    "         x = func(x, y);\n"
-                    "         if (x == 10*y) then {\n"
-                    "             x = 5;\n"
-                    "         }\n"
-                    "         while (x < 10) {\n"
-                    "             x = x + 1;\n"
-                    "         }\n"
-                    "         return 0;\n"
-                    "     }\n"
-                    " }\n", &ast),
+        ParseString(
+            " program {\n"
+            "     void main() {\n"
+            "         integer x = 0;\n"
+            "         integer y = 100 + 32;\n"
+            "         x = 10;\n"
+            "         x = func(x, y);\n"
+            "         if (x == 10*y) then {\n"
+            "             x = 5;\n"
+            "         }\n"
+            "         while (x < 10) {\n"
+            "             x = x + 1;\n"
+            "         }\n"
+            "         return 0;\n"
+            "     }\n"
+            " }\n",
+            &ast),
         0);
 
-
-    AstNodeFull_List body = Ast_full_block(ast, Ast_full_meth_decl(ast, Ast_full_prog(ast).begin).body);      
-    ASSERT_EQ(body.end- body.begin, 7);
+    AstNodeFull_List body = Ast_full_block(
+        ast, Ast_full_meth_decl(ast, Ast_full_prog(ast).begin).body);
+    ASSERT_EQ(body.end - body.begin, 7);
 }
 
 TEST_F(ParserTest, ParseArithExpression) {
     Ast ast;
     ASSERT_EQ(ParseString("program { integer x = 1 + 2 * 3; }", &ast), 0);
 
-    AstNodeFull_VarDecl var_decl = Ast_full_var_decl(ast, Ast_full_prog(ast).begin);
+    AstNodeFull_VarDecl var_decl =
+        Ast_full_var_decl(ast, Ast_full_prog(ast).begin);
     ASSERT_EQ(var_decl.ident, StrPool_put(&str_pool, "x"));
     ASSERT_EQ(var_decl.type, Type_INT);
 
@@ -202,15 +206,17 @@ TEST_F(ParserTest, ParseArithExpression) {
     ASSERT_EQ(binop.op, BinOp_ADD);
     ASSERT_EQ(Ast_full_binop(ast, binop.rhs).op, BinOp_MUL);
 
-    ASSERT_NE(ParseString("program { void f() { integer x = 10 + ; }}", &ast), 0);
+    ASSERT_NE(
+        ParseString("program { void f() { integer x = 10 + ; }}", &ast), 0);
     ASSERT_NE(ParseString("program { void f() { integer x = ; }}", &ast), 0);
 }
 
 TEST_F(ParserTest, ParseBoolExpressions) {
     Ast ast;
     ASSERT_EQ(
-        ParseString(" program { bool result = true && false; }\n", &ast),0);
+        ParseString(" program { bool result = true && false; }\n", &ast), 0);
 
-    AstNodeFull_BinOp binop = Ast_full_binop(ast, Ast_full_var_decl(ast, Ast_full_prog(ast).begin).init_expr);
+    AstNodeFull_BinOp binop = Ast_full_binop(
+        ast, Ast_full_var_decl(ast, Ast_full_prog(ast).begin).init_expr);
     ASSERT_EQ(binop.op, BinOp_AND);
 }
