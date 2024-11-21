c8dc02a8288258a435f08d6531c59f530b8abad0
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2024-11-21 15:50:00 -0300

    generate function prologue

--- src/asm.c
+++ src/asm.c
@@ -3,22 +3,43 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "asm.h"
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+const char *CC_REGS[] = {"edi", "esi", "edx", "ecx", "r8d", "r9d"};
+uint32_t CC_REGS_SIZE = 6;
+uint32_t REG_SIZE = 8;
+
+
+static void gen_asm_label(const StrIdx label, StrPool strs, FILE *file) {
+    const char *label_str = StrPool_get(&strs, label);
+    fprintf(file, "%s:\n", label_str);
+}
 
 static void gen_asm_func(const Func func, StrPool strs, FILE *file) {
-    const char *func_name = StrPool_get(&strs, func.name);
-    fprintf(file, "%s:\n// TODO\n", func_name);
+    gen_asm_label(func.name, strs, file);
+    fprintf(file, "\tenter $(%d*%d), $0\n", REG_SIZE, func.locals);
+
+    int32_t stack_idx = -REG_SIZE;
+    for (int32_t i = MIN(CC_REGS_SIZE, func.arity) - 1; i >= 0; i--) {
+        fprintf(file, "\tmov %s %d(%%rbp)\n", CC_REGS[i], stack_idx);
+        stack_idx -= REG_SIZE;
+    }
+
     return;
 }
 
 static void gen_asm_globls(const GlobalVec globls, StrPool strs, FILE *file) {
-    fprintf(file, "\t.text\n");
+    if (globls.len == 0) {
+        return;
+    }
+
+    fprintf(file, "\t.data\n");
     for (uint32_t i = 0; i < globls.len; i++) {
         Global globl = globls.elems[i];
         const char *globl_name = StrPool_get(&strs, globl.name);
 
         fprintf(file,
             "\t.globl %s\n"
-            "\t.data\n"
             "\t.align 4\n"
             "\t.type %s, @object"
             "\n\t.size %s, 4\n",
